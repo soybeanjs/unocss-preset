cd2c6ebb7f14502ae4a815a41ac7d8eacc85f424
Soybean <soybeanjs@outlook.com>
2025-07-09 00:30:48 +0800

    chore(projects): replace tsup with tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  platform: 'node',
+  clean: true,
+  dts: true,
+  sourcemap: false,
+  minify: true,
+  format: ['esm'],
+  target: 'node18'
+});
